e93c2ceb8abff4b51a824ac329f68982df94bb26
Stanley Winata <winatastanley355@gmail.com>
2025-12-27 18:54:56 +0700

    feat: Axum route

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 mod envs;
+use axum::{Router, routing::get};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenvy::dotenv().ok();
-    println!("Hello, world!");
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let host_address = envs::Envs::host_address();
+    let listener = match tokio::net::TcpListener::bind(&host_address).await {
+        Ok(listen) => listen,
+        Err(err) => panic!("Failed to bind to address: {}", err),
+    };
+
+    println!("Server started at {}", &host_address);
+    match axum::serve(listener, app).await {
+        Ok(_) => println!("Server started successfully"),
+        Err(err) => panic!("Failed to start server: {}", err),
+    }
 }
