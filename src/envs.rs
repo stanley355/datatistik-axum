149fe8e25627822b048eb1ae4b7649958f53926a
Stanley Winata <winatastanley355@gmail.com>
2026-05-15 11:34:38 +0700

    fix: Env

--- src/envs.rs
+++ src/envs.rs
@@ -27,6 +27,8 @@ impl Envs {
 
     pub fn trusted_domains() -> String {
         let expect = format!("{} must be set", Self::TRUSTED_DOMAINS);
-        std::env::var(Self::APP_ENV).expect(&expect).to_string()
+        std::env::var(Self::TRUSTED_DOMAINS)
+            .expect(&expect)
+            .to_string()
     }
 }
