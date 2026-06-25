d8c5f5a9d09c58b5f92bc5e3a873fcadf1ebf69a
Stanley Winata <winatastanley355@gmail.com>
2026-06-25 13:34:12 +0700

    Carts migration

---
+++ migrations/2026-06-25-062920-0000_carts/up.sql
@@ -0,0 +1,12 @@
+-- Your SQL goes here
+CREATE TABLE carts (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+    amount INTEGER NOT NULL DEFAULT 1
+);
+
+-- Register the table with Diesel's internal trigger helper
+SELECT diesel_manage_updated_at('carts');
